d00caaee06aa054173bc80580459d00e33fd25e3
Alessandro Oliveira <alessandro.ibes@gmail.com>
2021-04-03 20:30:39 -0300

    Service Locator "Pattern" (Não recomendado)

---
+++ DemoDI/Controllers/ServiceLocator2Controller.cs
@@ -0,0 +1,16 @@
+﻿using DemoDI.Cases;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DemoDI.Controllers
+{
+    public class ServiceLocator2Controller : Controller
+    {
+        public void Index([FromServices] IServiceProvider serviceProvider)
+        {
+            // Retorna null se não estiver registrado
+            serviceProvider.GetRequiredService<IClienteServices>().AdicionarCliente(new Cliente());
+        }
+    }
+}
